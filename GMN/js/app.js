c177a03e424293f76a78eff148be03772a7806d1
Marahimimv <112489559+Marahimimv@users.noreply.github.com>
2022-11-20 19:02:39 +0400

    Add files via upload

---
+++ GMN/js/app.js
@@ -0,0 +1,15 @@
+// function sahe(r){
+//     var a = Math.PI * r *r;
+//     return "Sahesi: " + a;
+//     }
+//     console.log(sahe(2))
+    
+//     function peri(r){
+//     var p = 2 * Math.PI * r;
+//     return "Perimetri: " + p;
+//     }
+//     console.log(peri(3))
+
+
+var Date = new Date().getHours();
+
